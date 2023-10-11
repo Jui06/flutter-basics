38b3b0033159d645f457044f5e72e666193f6ea8
juie k <juie@gmail.com>
2023-10-11 11:00:31 +0530

    voting

---
+++ Vote/Vote.dart
@@ -0,0 +1,20 @@
+void main()
+{
+	int age=1;
+	if(age<18)
+	{
+	
+		if(age<0)
+		{
+		print("please enter valid age");
+		}
+		else
+		{
+		print("age should be greater than 18");
+		}
+
+
+}
+		else
+		print("valid age..go for voting");
+}
